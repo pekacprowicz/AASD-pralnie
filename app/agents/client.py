31de7210a3156e3ee9ce6ccc7764bdde7b55f5d9
Przemyslaw Kacprowicz <przemyslaw.kacprowicz.stud@pw.edu.pl>
2021-01-03 23:43:04 +0100

    Most of timetable check

--- app/agents/client.py
+++ app/agents/client.py
@@ -1,6 +1,6 @@
 import asyncio
 from spade.agent import Agent
-from spade.behaviour import OneShotBehaviour
+from spade.behaviour import FSMBehaviour, State
 from constants.agents import Agents
 from spade.message import Message
 from utils.messaging import Messaging
@@ -9,29 +9,85 @@ import pathlib
 
 class Client(Agent):
     
-    class CreateReservationBehav(OneShotBehaviour):
+    class CreateReservationBehav(FSMBehaviour):
+        async def on_start(self):
+            print(f"Client CreateReservationBehav starting at initial state {self.current_state}")
 
-        def __init__(self, dates_list):
-            super().__init__()
-            self.dates_list = dates_list
+        async def on_end(self):
+            print(f"Client CreateReservationBehav finished at state {self.current_state}")
+            await self.agent.stop()
 
+    class UserPenaltiesVerificationState(State):
         async def run(self):
-            # dates_list = self.dates_priority_list.copy()
-            metadata = {"type": "UserVerification"}
+            metadata = {"type": "UserPenaltiesVerification"}
             msg = Messaging.prepare_message(Agents.SUPERVISOR, "", **metadata)
             
             await self.send(msg)
             print("Message sent!")
 
-            self.exit_code = "Finished"
+            self.set_next_state("SendDateProposal")
 
-            await self.agent.stop()
+    class SendDateProposalState(State):
+        async def run(self):
+            msg = await self.receive(timeout=10)
+            if msg:
+                response_status = msg.get_metadata("status")
+
+                if response_status == "accepted":
+                    possible_dates = self.get_dates_with_priority()
+                    metadata = {"type": "DatetimeProposal"}
+                    
+                    for date in possible_dates:
+                        msg = Messaging.prepare_message(Agents.TIMETABLE, date, **metadata)
+                        print(f"Sending message")
+                        await self.send(msg)
+                        self.set_next_state("DateProposalResponse")
+
+                else:
+                    self.exit_code("User cannot reserve machine due penalties")
+                    await self.agent.stop()
+
+            else:
+                print(f"Client's {self.name} VerifyUser Behaviour hasn't received any message")
+
+    class DateProposalResponseState(State):
+        async def run(self):
+            msg = await self.receive(timeout=10)
+            if msg:
+                response_status = msg.get_metadata("status")
+
+                possible_dates = self.get_dates_with_priority()
+                metadata = {"type": "DatetimeProposal"}
+                
+                for date in possible_dates:
+                    msg = Messaging.prepare_message(Agents.TIMETABLE, date, **metadata)
+                    print(f"Sending message")
+                    await self.send(msg)
+
+            else:
+                print(f"Client's {self.name} VerifyUser Behaviour hasn't received any message")
+
+    def init_create_reservation_behaviour(self):
+        verify_msg_template = Template()
+        verify_msg_template.set_metadata("type", "UserPenaltiesVerificationResponse")
+        # verify_msg_template.set_metadata("type", "UserPenaltiesVerificationResponse")
+        vu_behav = self.VerifyUserBehav()
+        fsm = self.CreateReservationBehav()
+        fsm.add_state(name="UserPenaltiesVerificationState", state=self.UserPenaltiesVerificationState(), initial=True)
+        fsm.add_state(name="SendDateProposal", state=self.SendDateProposalState())
+        fsm.add_state(name="DateProposalResponse", state=self.DateProposalResponseState())
+        fsm.add_state(name="STATE_FOUR", state=self.PaymentResponseState())
+        fsm.add_state(name="STATE_FIVE", state=self.MachineAvailableState())
+        fsm.add_transition(source="STATE_ONE", dest="STATE_TWO")
+        fsm.add_transition(source="STATE_TWO", dest="STATE_THREE")
+        fsm.add_transition(source="STATE_THREE", dest="STATE_FOUR")
+        fsm.add_transition(source="STATE_FOUR", dest="STATE_FIVE")
+        self.add_behaviour(fsm)
 
     async def setup(self):
         self.db_connection = self.connect_to_local_db()
         self.db_init()
-        self.dates_priority_list = list()
-        self.create_res_behav = self.CreateReservationBehav(self.dates_priority_list)
+        self.create_res_behav = self.CreateReservationBehav()
         self.add_behaviour(self.create_res_behav)
 
     def connect_to_local_db(self):
@@ -49,11 +105,20 @@ class Client(Agent):
         return f"db/db_{self.name}.db"
 
     def db_init(self):
-        sql_create_prorities_table = """ CREATE TABLE IF NOT EXISTS prorities (
+        sql_create_prorities_table = """ CREATE TABLE IF NOT EXISTS priorities (
                                             id integer PRIMARY KEY,
-                                            datetime text NOT NULL,
-                                            prority integer NOT NULL
+                                            prefered_date text NOT NULL,
+                                            priority integer NOT NULL
                                     ); """
 
         crsr = self.db_connection.cursor()
         crsr.execute(sql_create_prorities_table)
+
+    def get_dates_with_priority(self):
+        sql_get_user_penalties = f" SELECT * FROM priorities; "
+
+        crsr = self.db_connection.cursor()
+        crsr.execute(sql_create_penalties_table)
+        dates_with_priorities = crsr.fetchall()
+
+        return dates_with_priorities
--- app/agents/supervisor.py
+++ app/agents/supervisor.py
@@ -1,27 +1,36 @@
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.template import Template
+from utils.messaging import Messaging
 import pathlib
 import sqlite3
 
 class Supervisor(Agent):
 
-    class VerifyUser(CyclicBehaviour):
+    class VerifyUserBehav(CyclicBehaviour):
         async def run(self):
             msg = await self.receive(timeout=10)
             if msg:
-                msg_type = msg.get_metadata("type")
-                print(f"Incoming msg_type: {msg_type}")
+                username = msg.sender.localpart
+                metadata = {"type": "UserPenaltiesVerificationResponse"}
+
+                if self.search_for_active_penalties(username) > 0:
+                    metadata["status"] = "rejected"
+                else:
+                    metadata["status"] = "accepted"
+                
+                response = Messaging.prepare_message(msg.sender, "", **metadata)
+                await self.send(response)
             else:
-                print(f"No message...")
+                print(f"Supervisor's VerifyUser Behaviour hasn't received any message")
 
 
     async def setup(self):
         self.db_connection = self.connect_to_local_db()
         self.db_init()
         verify_msg_template = Template()
-        verify_msg_template.set_metadata("type", "UserVerification")
-        vu_behav = self.VerifyUser()
+        verify_msg_template.set_metadata("type", "UserPenaltiesVerification")
+        vu_behav = self.VerifyUserBehav()
         self.add_behaviour(vu_behav, verify_msg_template)
 
     def connect_to_local_db(self):
@@ -48,3 +57,15 @@ class Supervisor(Agent):
 
         crsr = self.db_connection.cursor()
         crsr.execute(sql_create_penalties_table)
+
+    def search_for_active_penalties(self, username):
+        sql_get_user_penalties = f""" SELECT COUNT(end_date) FROM penalties as p
+                                        WHERE p.user = \"{username}\"
+                                        AND datetime(p.end_date) > datetime('now');
+                                    ); """
+
+        crsr = self.db_connection.cursor()
+        crsr.execute(sql_create_penalties_table)
+        active_penalties = int(crsr.fetchall())
+
+        return active_penalties
