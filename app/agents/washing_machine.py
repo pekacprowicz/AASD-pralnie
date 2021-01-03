6131b4ca2886bed245ff7786ba071ab3fd29e0cd
Mateusz Szychiewicz <szychiewiczmateusz@gmail.com>
2021-01-03 20:00:34 +0100

    washing machine finite state machine

---
+++ app/agents/washing_machine.py
@@ -0,0 +1,60 @@
+import time
+
+from spade.agent import Agent
+from spade.message import Message
+from utils.messaging import Messaging
+from spade.behaviour import FSMBehaviour, State
+
+STATE_FREE = "STATE_FREE"
+STATE_AUTH = "STATE_AUTH"
+STATE_WORKING = "STATE_WORKING"
+
+class WashingMachineFSMBehaviour(FSMBehaviour):
+    async def on_start(self):
+        print(f"Washing machine starting at initial state {self.current_state}")
+
+    async def on_end(self):
+        print(f"Washing machine finished at state {self.current_state}")
+        await self.agent.stop()
+
+class StateFree(State):
+    async def run(self):
+        print("I'm at state free")
+        msg = await self.receive(timeout=10)
+        if msg:
+            msg_type = msg.get_metadata("type")
+            print(f"Incoming msg_type: {msg_type}")
+            if msg_type == "GrantAccess":
+                self.set_next_state(STATE_AUTH)
+
+        print(f"No message...")
+        self.set_next_state(STATE_FREE)
+        
+class StateAuth(State):
+    async def run(self):
+        print("I'm at state auth")
+        metadata = {"type": "AccessGranted"}
+        msg = Messaging.prepare_message(Agents.SUPERVISOR, "", **metadata)
+            
+        await self.send(msg)
+        print("Message sent!")
+        
+        self.set_next_state(STATE_WORKING)
+
+class StateWorking(State):
+    async def run(self):
+        print("I'm at state working")
+        sleep(10)
+        self.set_next_state(STATE_FREE)
+
+class WashingMachine(Agent):
+    async def setup(self):
+        fsm = WashingMachineFSMBehaviour()
+        fsm.add_state(name=STATE_FREE, state=StateFree(), initial=True)
+        fsm.add_state(name=STATE_AUTH, state=StateAuth())
+        fsm.add_state(name=STATE_WORKING, state=StateWorking())
+        fsm.add_transition(source=STATE_FREE, dest=STATE_FREE)
+        fsm.add_transition(source=STATE_FREE, dest=STATE_AUTH)
+        fsm.add_transition(source=STATE_AUTH, dest=STATE_WORKING)
+        fsm.add_transition(source=STATE_WORKING, dest=STATE_FREE)
+        self.add_behaviour(fsm)
